1de5848421c7e754151ae8bad3af6825145b605c
GTiazara tiazara <tiazaragaetan@gmail.com>
2023-03-14 10:58:19 +0000

    ci menu server remove error

--- src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
+++ src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MenuServegdgsrApplication {
+public class MenuServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MenuServerApplication.class, args);
